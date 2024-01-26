a16054c25935ae65dd0535ea9cf4a2c6888fa74c
Sasha Di Figlia <difiglia95@hotmail.it>
2024-01-27 00:47:37 +0100

    added basic movement

---
+++ GGJP2024/Assets/Scripts/Controllers/AIController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AiController", menuName = "InputController/AiController")] 
+public class AiController : InputController
+{
+    public override bool RetrieveJumpInput()
+    {
+        return true;
+    }
+
+    public override float RetrieveMoveInput()
+    {
+        return 1f;
+    }
+}
