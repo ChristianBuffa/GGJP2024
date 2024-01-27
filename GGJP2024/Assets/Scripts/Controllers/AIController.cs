c89674e0f5c6da86f298307ea74682de890f1bf4
Sasha Di Figlia <difiglia95@hotmail.it>
2024-01-27 10:26:00 +0100

    added crouch

--- GGJP2024/Assets/Scripts/Controllers/AIController.cs
+++ GGJP2024/Assets/Scripts/Controllers/AIController.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AiController", menuName = "InputController/AiController")] 
 public class AiController : InputController
 {
+    public override bool RetrieveCrouchInput()
+    {
+        return false;
+    }
+
     public override bool RetrieveJumpInput()
     {
         return true;
